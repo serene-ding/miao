2a0815882edb339424f8850c12907a6805b38fd2
serene <sereneding123@gmail.com>
2022-01-11 21:38:01 +0800

    m

--- lodash/serene_ding-lodash.js
+++ lodash/serene_ding-lodash.js
@@ -1,64 +1,64 @@
 var serene_ding = {
-    chunk : function(array,size=1){
+    chunk: function(array, size = 1) {
         var res = []
         var j = 0
-        while(j < array.length){
+        while (j < array.length) {
             var one_chunk = []
-            for (var i = 0; i < size; i++){
+            for (var i = 0; i < size; i++) {
                 one_chunk[i] = array[j]
-                
+
                 j++
-                if(j == array.length){
+                if (j == array.length) {
                     break
                 }
             }
             res.push(one_chunk)
         }
-        return res      
+        return res
     },
-    compact : function(array){
+    compact: function(array) {
         var res = []
-        for (var i = 0; i < array.length; i++){
-            if(array[i]){
-               res.push(array[i])
+        for (var i = 0; i < array.length; i++) {
+            if (array[i]) {
+                res.push(array[i])
             }
         }
         return res
     },
 
-    difference: function(array,...values){
+    difference: function(array, ...values) {
         var res = []
-        for (var i = 0; i < array.length; i++){
+        for (var i = 0; i < array.length; i++) {
             var item = array[i]
             var equal = false
-            for (var j = 0; j < values.length; j++){
-                for (var k = 0; k < values[j].length; k++){
-                    if (item == values[j][k]){
+            for (var j = 0; j < values.length; j++) {
+                for (var k = 0; k < values[j].length; k++) {
+                    if (item == values[j][k]) {
                         equal = true
                     }
                 }
             }
-            if (!equal){
+            if (!equal) {
                 res.push(item)
             }
-            
+
         }
         return res
     },
 
-    concat: function(array,...values){
+    concat: function(array, ...values) {
         //copy the array
         var c_array = []
-        for (var k = 0; k < array.length; k++){
+        for (var k = 0; k < array.length; k++) {
             c_array[k] = array[k]
         }
         //concat
-        for (var i = 0; i < values.length; i++){
+        for (var i = 0; i < values.length; i++) {
             let item = values[i]
-            if (typeof(item) == 'number'){
+            if (typeof(item) == 'number') {
                 c_array.push(item)
-            }else {
-                for (var j = 0; j < item.length; j++){
+            } else {
+                for (var j = 0; j < item.length; j++) {
                     c_array.push(item[j])
                 }
             }
@@ -66,27 +66,27 @@ var serene_ding = {
         return c_array
     },
 
-    drop: function(array,n=1){
+    drop: function(array, n = 1) {
         var res = []
-        for (var i = n, j = 0; i < array.length; i++,j++){
+        for (var i = n, j = 0; i < array.length; i++, j++) {
             res[j] = array[i]
         }
         return res
     },
 
-    dropRight: function(array,n=1){
+    dropRight: function(array, n = 1) {
         var res = []
-        for (var i = 0, j = 0; i < array.length - n; i++,j++){
+        for (var i = 0, j = 0; i < array.length - n; i++, j++) {
             res[j] = array[i]
         }
         return res
     },
 
-    dropRightwhile: function(array,f){
+    dropRightwhile: function(array, f) {
         var n = 0
         var i = 0
-        while (i < array.length){
-            if(!f(array[i])){
+        while (i < array.length) {
+            if (!f(array[i])) {
                 break
             }
             i++
@@ -94,119 +94,119 @@ var serene_ding = {
         }
         //drop_right
         var res = []
-        for (var i = 0, j = 0; i < array.length - n; i++,j++){
+        for (var i = 0, j = 0; i < array.length - n; i++, j++) {
             res[j] = array[i]
         }
         return res
     },
 
-    fill: function(array, value, start=0, end=array.length){
-        if (arguments.length == 2){
-            for (var i = 0; i < array.length; i++){
+    fill: function(array, value, start = 0, end = array.length) {
+        if (arguments.length == 2) {
+            for (var i = 0; i < array.length; i++) {
                 array[i] = value
             }
-        }else if(arguments.length == 3){
-            for (var i = start; i < array.length; i++){
+        } else if (arguments.length == 3) {
+            for (var i = start; i < array.length; i++) {
                 array[i] = value
             }
-        }else{
-            for (var i = start; i < end; i++){
+        } else {
+            for (var i = start; i < end; i++) {
                 array[i] = value
             }
         }
         return array
     },
     //Array.isArray()
-    findIndex: function(array, f, fromIndex = 0){
-        if(typeof f == 'function'){
-            for (var i = fromIndex; i < array.length; i++){
+    findIndex: function(array, f, fromIndex = 0) {
+        if (typeof f == 'function') {
+            for (var i = fromIndex; i < array.length; i++) {
                 var item = array[i]
-                if(f(item)){
+                if (f(item)) {
                     return i
                 }
             }
-        }else if(typeof f == 'string'){
+        } else if (typeof f == 'string') {
             var property = f
-            var f = function(item){
+            var f = function(item) {
                 return item[property]
             }
-            for (var i = fromIndex; i < array.length; i++){
+            for (var i = fromIndex; i < array.length; i++) {
                 var item = array[i]
-                if(f(item)){
+                if (f(item)) {
                     return i
                 }
-            } 
-        }else if(Array.isArray(f)){
-            
+            }
+        } else if (Array.isArray(f)) {
+
             var property = f[0]
             var value = f[1]
-            var f = function(item){
+            var f = function(item) {
                 return item[property] == value
             }
-            for (var i = fromIndex; i < array.length; i++){
+            for (var i = fromIndex; i < array.length; i++) {
                 var item = array[i]
-                if(f(item)){
+                if (f(item)) {
                     return i
                 }
-            } 
-        }else{
-            
+            }
+        } else {
+
         }
     },
-    flatten: function(array){
+    flatten: function(array) {
         var res = []
-        for (var i = 0; i < array.length; i++){
+        for (var i = 0; i < array.length; i++) {
             var item = array[i]
-            if (Array.isArray(item)){
-                
-                for (var j = 0; j < item.length; j++){
+            if (Array.isArray(item)) {
+
+                for (var j = 0; j < item.length; j++) {
                     res.push(item[j])
                 }
-            }else{
+            } else {
                 res.push(item)
             }
         }
         return res
     },
-    flattenDeep: function(array){
+    flattenDeep: function(array) {
         var res = []
-    for (var i = 0; i < array.length; i++){
-        var item = array[i]
-        if (Array.isArray(item)){
-            
-            var temp = serene_ding.flattenDeep(item)
-            for (var j = 0; j < temp.length; j++){
-                res.push (temp[j])
+        for (var i = 0; i < array.length; i++) {
+            var item = array[i]
+            if (Array.isArray(item)) {
+
+                var temp = serene_ding.flattenDeep(item)
+                for (var j = 0; j < temp.length; j++) {
+                    res.push(temp[j])
+                }
+            } else {
+                res.push(item)
             }
-        }else{
-            res.push(item)
         }
-    }
-    return res
+        return res
     },
-    head: function(array){
+    head: function(array) {
         return array[0]
     },
 
-    indexOf: function(array, value, fromIndex=0){
-        for (var i = 0; i < array.length; i++){
-            if (array[i] == value){
+    indexOf: function(array, value, fromIndex = 0) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] == value) {
                 return i
             }
         }
         return -1
-    
+
     },
-    initial: function(array){
+    initial: function(array) {
         var ini = []
-        for (var i = 0; i < array.length - 1; i++){
+        for (var i = 0; i < array.length - 1; i++) {
             ini[i] = array[i]
         }
         return ini
     },
-    fromPairs:function(pairs){
+    fromPairs: function(pairs) {
         var res = {}
-        for (var i = 0; i < pairs.length; i++){
+        for (var i = 0; i < pairs.length; i++) {
             var one_pair = pairs[i]
             var pro = one_pair[0]
             var value = one_pair[1]
@@ -214,26 +214,26 @@ var serene_ding = {
         }
         return res
     },
-    join: function(array, separator=','){
+    join: function(array, separator = ',') {
         var res = ''
-        for (var i = 0; i < array.length; i++){
-            if(i==array.length - 1){
+        for (var i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
                 res += array[i]
-            }else{
+            } else {
                 array[i] += ''
                 res += array[i] + separator
             }
         }
         return res
     },
-    last: function(array){
-        for(var i = 0; i < array.length; i++){
-            if (i == array.length - 1){
+    last: function(array) {
+        for (var i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
                 return array[i]
             }
         }
     },
-     
-        
-    
+
+
+
 }
